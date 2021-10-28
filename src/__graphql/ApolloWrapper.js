0d993f3e9777a5c7c0e7e5d67e66890d42bc07a1
Sean <>
2021-10-28 14:29:19 -0700

    deleted apolloClient.js within __graphql

--- src/__graphql/ApolloWrapper.js
+++ src/__graphql/ApolloWrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import client from 'src/__graphql/apolloClient_and_queries';
-// import { client } from "src/__apollo/client"
+
 
 export const ApolloWrapper = ({ children }) => (
   <ApolloProvider client={client}>{children}</ApolloProvider>
