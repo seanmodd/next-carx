eea2e5f589c67dbda41668250c264be6ae7821f4
Sean <>
2021-10-27 20:59:25 -0700

    just changed syntax of apollo imports

--- src/apollo/ApolloWrapper.js
+++ src/apollo/ApolloWrapper.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { ApolloProvider } from "@apollo/client"
-import { client } from "./client"
+import { client } from "src/apollo/client"
 
 export const ApolloWrapper = ({ children }) => {
   return <ApolloProvider client={client}>{children}</ApolloProvider>
