5bb19da49f20736600d50bb44807e505cd60269c
Sean <>
2021-10-30 15:52:34 -0700

    webpack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  webpack5: false,
 };
