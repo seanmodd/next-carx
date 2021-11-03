d45cc5dd0f8e7eec362d5dc92740daa1d067be15
Sean <>
2021-11-03 12:49:51 -0700

    commented out a lot of next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
-const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
+// const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
 
 module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   webpack5: false,
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      };
-    }
+  // webpack: (config, { isServer }) => {
+  //   // Fixes npm packages that depend on `fs` module
+  //   if (!isServer) {
+  //     config.node = {
+  //       fs: 'empty',
+  //     };
+  //   }
 
-    return config;
-  },
-  resolve: {
-    plugins: [PnpWebpackPlugin],
-  },
-  resolveLoader: {
-    plugins: [PnpWebpackPlugin.moduleLoader(module)],
-  },
+  //   return config;
+  // },
+  // resolve: {
+  //   plugins: [PnpWebpackPlugin],
+  // },
+  // resolveLoader: {
+  //   plugins: [PnpWebpackPlugin.moduleLoader(module)],
+  // },
 };
